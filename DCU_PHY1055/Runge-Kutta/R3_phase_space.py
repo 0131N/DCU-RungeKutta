58dcf253d2d79a7c56c23793c284d2af15f0e4c3
0131N <oisin.corrigan6@mail.dcu.ie>
2024-09-27 18:49:36 +0100

    Update R3_all\

--- DCU_PHY1055/Runge-Kutta/R3_phase_space.py
+++ DCU_PHY1055/Runge-Kutta/R3_phase_space.py
@@ -1,22 +1,59 @@
-# code courtesy of Adam Dempsey
-# modified for PHY1055 by Oisín Creaner
+import numpy as np
 import matplotlib.pyplot as plt
+from scipy import integrate
+from pathlib import Path
 
+def simple_pendulum(t, y):
+    x, v = y
+    dydt = np.array([v, -x])
+    return dydt
 
-def phase_space():
-    # when you merge this into R3_template, you will use the values of v and x in that program
-    v = []
-    x = []
+def generate_path(home_folder=str(Path.home()), subfolder='/Documents/', basename='output', extension='png'):
+    output_folder = home_folder + subfolder
+    filename = basename + '.' + extension
+    output_path = output_folder + filename
+    return output_path
 
-    # create a plot
+def phase_space(x, v):
     plt.figure()
-
-    # plots v against x in black ('k')
     plt.plot(v, x, 'k')
-
-    # sets the axes to equal sizes
     plt.axis('equal')
-
-    # labels the axes
     plt.xlabel(r"$x$")
     plt.ylabel(r"$v$")
+
+
+def plot_time_dependency(t, x, v):
+    plt.figure()
+    plt.plot(t, x, label=r"$x(t)$")
+    plt.plot(t, v, label=r"$v(t)$")
+    plt.xlabel("Time (t)")
+    plt.ylabel("Position (x) and Velocity (v)")
+
+    plt.legend(loc=1)
+
+def main():
+    x0 = 0
+    v0 = 1
+    y0 = (x0, v0)
+    t0 = 0
+    tf = 10*np.pi
+    n = 1001
+    t = np.linspace(t0, tf, n)
+
+    result = integrate.solve_ivp(fun=simple_pendulum, t_span=(t0, tf), y0=y0, method="RK45", t_eval=t)
+    x, v = result.y
+    t = result.t
+
+    # Create a figure with two subplots
+    fig, axs = plt.subplots(2)
+    plot_time_dependency(t, x, v)
+    phase_space(x, v)
+    plt.tight_layout()
+
+    filename = generate_path(basename='Harmonic-init', extension='png')
+    plt.savefig(filename, bbox_inches='tight')
+    print("Output file saved to {}.".format(filename))
+    plt.show()
+
+if __name__ == '__main__':
+    main()
