00e35855f0fe1006896df1a6f49b74994605f658
0131N <oisin.corrigan6@mail.dcu.ie>
2024-09-27 18:50:24 +0100

    R5 finished

--- DCU_PHY1055/Runge-Kutta/R5_excerpt.py
+++ DCU_PHY1055/Runge-Kutta/R5_excerpt.py
@@ -1,34 +1,65 @@
-# code courtesy of Adam Dempsey
-# modified for PHY1055 by Oisín Creaner
-# THIS ISN'T WORKING CODE. IT ONLY WORKS IF YOU CORRECTLY INCORPORATE IT INTO YOUR CODE
+import numpy as np
+import matplotlib.pyplot as plt
 from scipy import integrate
+from pathlib import Path
 
-
-# PLACEHOLDER FUNCTION
 def damped_pendulum(t, y, b, omega0):
-    """
-    The proper version of this function is defined in R4_function.
-    What's here is just a placeholder.
-    """
-    pass
+    x, v = y
+    dxdt = v
+    dvdt = -b*v-(omega0**2)*x
+    dydt = np.array([dxdt, dvdt])
+    return dydt
 
+def generate_path(home_folder=str(Path.home()), subfolder='/Documents/', basename='output', extension='png'):
+    output_folder = home_folder + subfolder
+    filename = basename + '.' + extension
+    output_path = output_folder + filename
+    return output_path
 
-# PLACEHOLDER VARIABLES
-# these don't do anything.  When you incorporate the code into your program
-# these will be defined elsewhere in your code
-t, y, b, omega0 = 0
-t0, tf, y0 = 0
+def phase_space(x, v):
+    plt.figure()
+    plt.plot(v, x, 'k')
+    plt.axis('equal')
+    plt.xlabel(r"$x$")
+    plt.ylabel(r"$v$")
 
-# below this line are the bits that you need to include
-#######################################################
+def plot_time_dependency(t, x, v):
+    plt.figure()
+    plt.plot(t, x, label=r"$x(t)$")
+    plt.plot(t, v, label=r"$v(t)$")
+    plt.xlabel("Time (t)")
+    plt.ylabel("Position (x) and Velocity (v)")
+    plt.legend(loc=1)
 
-# define a lambda function in a proper program that takes these arguments
-lfun = lambda t, y, : damped_pendulum(t, y, b, omega0)
+def main():
+    x0 = 0
+    v0 = 1
+    y0 = (x0, v0)
+    t0 = 0
+    tf = 10*np.pi
+    n = 1001
+    t = np.linspace(t0, tf, n)
+    b = 0.5  # damping coefficient
+    omega0 = 1.2  # resonant frequency
 
+    lfun = lambda t, y: damped_pendulum(t, y, b, omega0)
 
-# The part of the code running the solver then needs to read:
-result = integrate.solve_ivp(fun=lfun,  # The function defining the derivative
-                             t_span=(t0, tf),  # Initial and final times
-                             y0=y0,  # Initial state
-                             method="RK45",  # Integration method
-                             t_eval=t)  # Time points for result to be defined at
+    result = integrate.solve_ivp(fun=lfun,  # The function defining the derivative
+                                 t_span=(t0, tf),  # Initial and final times
+                                 y0=y0,  # Initial state
+                                 method="RK45",  # Integration method
+                                 t_eval=t)  # Time points for result to be defined at
+
+    x, v = result.y
+    t = result.t
+
+    plot_time_dependency(t, x, v)
+    phase_space(x, v)
+
+    filename = generate_path(basename='Damped-Pendulum', extension='png')
+    plt.savefig(filename, bbox_inches='tight')
+    print("Output file saved to {}.".format(filename))
+    plt.show()
+
+if __name__ == '__main__':
+    main()
